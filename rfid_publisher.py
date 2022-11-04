e9011d00188a75d98cfe67f65ac37662bd641adf
BrennanDrake <81040749+BrennanDrake@users.noreply.github.com>
2022-11-04 14:12:26 -0400

    Reorganize
    Reorganizing

---
+++ rfid_publisher.py
@@ -0,0 +1,23 @@
+ #!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+ 
+def talker():
+    pub = rospy.Publisher('/rfid', String, queue_size=10)
+    rospy.init_node('rfid', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+
+    ser = serial.Serial("/dev/ttyACM0", 115200, timeout=1)
+        ser.reset_input_buffer()
+
+    while not rospy.is_shutdown():
+        if ser.in_waiting > 0:
+            id = ser.readline().decode('utf-8').rstrip()
+            pub.publish(id)
+            rate.sleep()
+   
+if __name__ == '__main__':
+    try:
+       talker()
+    except rospy.ROSInterruptException:
+       pass
