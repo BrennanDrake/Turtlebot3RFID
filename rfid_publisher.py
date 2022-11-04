e51640d1238f1a9f3044c1f776db256ab24d4971
BrennanDrake <81040749+BrennanDrake@users.noreply.github.com>
2022-11-04 17:08:07 -0400

    Update rfid_publisher.py

--- rfid_publisher.py
+++ rfid_publisher.py
@@ -12,13 +12,14 @@ def talker():
     ser = serial.Serial("/dev/ttyACM0", 115200, timeout=1)
 
     while not rospy.is_shutdown():
-        if ser.in_waiting > 0:
-            id = ser.readline()
-            #.decode('utf-8').rstrip()
-            cmd = String()
-            cmd.data = id
-            pub.publish(cmd)
-            rate.sleep()
+        # if ser.in_waiting > 0:
+        #     id = ser.readline().decode('utf-8').rstrip()
+        #     cmd = String()
+        #     cmd.data = id
+        #     pub.publish(cmd)
+        #     rate.sleep()
+        rospy.loginfo(ser)
+        rate.sleep()
    
 if __name__ == '__main__':
     try:
